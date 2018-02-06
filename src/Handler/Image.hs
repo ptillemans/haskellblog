bf1f3ca425f4a1c11a2ec589cc359737d62ddf90
Peter Tillemans <pti@melexis.com>
2018-02-06 08:14:25 +0100

    add image entity

---
+++ src/Handler/Image.hs
@@ -0,0 +1,12 @@
+module Handler.Image where
+
+import Import
+
+getImageR :: ImageId -> Handler Html
+getImageR imageId = error "Not yet implemented: getImageR"
+
+postImageR :: ImageId -> Handler Html
+postImageR imageId = error "Not yet implemented: postImageR"
+
+deleteImageR :: ImageId -> Handler Html
+deleteImageR imageId = error "Not yet implemented: deleteImageR"
